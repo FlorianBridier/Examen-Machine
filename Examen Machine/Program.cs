60f5b293abc410b0dc5bf476f41c6e3d86270f83
Florian Proth <florian.proth10@laposte.net>
2024-05-31 12:43:32 +0200

    Ajout du sous programme pour le calcul de la suite (Pas finaliser - pas encore fonctionnel)

--- Examen Machine/Program.cs
+++ Examen Machine/Program.cs
@@ -1,37 +1,55 @@
-﻿int a; int b;
+﻿static void Main()
+{
+    int a;  int b;
 
 //Saisi de l'entier de départ par l'utilisateur
 Console.WriteLine("Saisir l'entier de départ de la suite de Syracuse :");
-a= Convert.ToInt32(Console.ReadLine());
+a = Convert.ToInt32(Console.ReadLine());
 
 //Saisi de la taille de la suite 
 Console.WriteLine("Saisir le nombre d'éléments de la suite :");
-b= Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+//Appel du sous-programme qui va calculer les valeurs de la suite
+int[] suite = CalculSuite(a, b);
 
 
-// Création du tableau 
-int[] suite = new int[b];
-
-// Calcul des éléments de la suite de Syracuse
-suite[0] = a;
-
-//Calcul de la suite de Syracuse
-for (int i = 1; i < b; i++)
-{ 
-    if (suite[i-1] % 2 == 0)
-    {
-        suite[i] = suite[i-1] / 2;
-    }
-    else
-    {
-        suite[i] = suite[i-1] * 3 + 1;
-    }
-}
-
-//Affichage du tableau résultat 
-Console.WriteLine("Le tableau résultat contient :");
+    //Affichage du tableau résultat 
+    Console.WriteLine("Le tableau résultat contient :");
 for (int i = 0; i < b; i++)
 {
     Console.Write(suite[i] + " ");
 }
+}
+
+
+//Calcul de la suite de Syracuse
+static int[] CalculSuite(int a, int b)
+{
+    int[] suite = new int[b];
+    suite[0] = a;
+
+    for (int i = 1; i < b; i++)
+    {
+        if (suite[i - 1] % 2 == 0)
+        {
+            suite[i] = suite[i - 1] / 2;
+        }
+        else
+        {
+            suite[i] = 3 * suite[i - 1] + 1;
+        }
+    }
+
+    return suite;
+}
+
+
+
+
+
+
+
+
+
 
